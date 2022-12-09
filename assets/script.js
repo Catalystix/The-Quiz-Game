d05537b4731e275e8007400e9ce3e84286e5930a
Catalystix <dcode.green@gmail.com>
2022-12-09 16:47:32 -0500

    added question array

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,43 @@
 var startButton = document.querySelector('#start');
 var checkHighScore = document.querySelector('highScores');
 
+var answer = document.querySelector('#answers');
 var timeRemaining = document.querySelector('#timer');
-
+var score = 0;
 var timeLeft = 2;
+var pointsLost = 5;
+var questions = [
+    { Q: 'What is the name for the Jewish New Year?',
+    A: [
+            'Hanukkah',
+            'Yom Kippur',
+            'Kwanza',
+            'Rosh Hashanah',
+        ],
+        correct: "Hanukkah"
+    },
+    { Q: 'How many blue stripes are there on the U.S. flag?',
+    A: [
+        '6',
+        '7',
+        '13',
+        '0',
+        ],
+        correct: '13'
+    },
+    { Q: 'Which planet is the hottest?',
+    A: [
+        'Venus',
+        'Saturn',
+        'Mercury',
+        'Mars',
+    ],
+    Correct: '13'
+    }
+]
 
+function startquestions()
 
-startButton.addEventListener('click', function(_startTimer) {});
    
 function startTimer() {
     var timer = setInterval(function() {
@@ -22,10 +53,15 @@ function startTimer() {
             var name = document.createElement(userName);
             name.textContent = userName;
             document.body.div.appendchild(userName);
+            var score = document.createElement(newScore);
+            score.textContent = newScore;
+            document.body.div.appendchild(newScore);
         }
     }, 1000);
 }
 
+
+startButton.addEventListener('click', function(startTimer) {});
 // function runGetName() {
 //     // timeRemaining(prompt) = 'Enter Your Name';
 //     var userName = document.createElement("h1");
@@ -38,6 +74,7 @@ function startTimer() {
 
 
 startTimer()
+startquestions()
 
 // questions need to be an array === 3 false and 1 true
 
@@ -45,4 +82,4 @@ startTimer()
  
 // when the game is over or time runs out the loop ends. 
 
-// when the game is over the user is asked for initials and shows score. 
+// when the game is over the user is asked for initials/name and shows score. 
